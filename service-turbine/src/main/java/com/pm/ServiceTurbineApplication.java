6b57820e5d3efc7e349dc3ceb5ffb1cfeb0bc3bf
pengcheng <1600481475@qq.com>
2020-09-25 15:16:54 +0800

    service-turbine

---
+++ service-turbine/src/main/java/com/pm/ServiceTurbineApplication.java
@@ -0,0 +1,35 @@
+package com.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pengcheng
+ * @version V1.0
+ * @description
+ * @date 2020/08/21 17:39
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
+public class ServiceTurbineApplication {
+    /**
+     * http://localhost:8764/turbine.stream
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceTurbineApplication.class, args );
+    }
+}
